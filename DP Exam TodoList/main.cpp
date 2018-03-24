e9e261a308cd395d00603edcbb3d9e5a463c1f8d
Kristian Sakarisson <kristian@sakarisson.com>
2018-03-24 11:59:53 +0200

    Added TaskList

--- DP Exam TodoList/main.cpp
+++ DP Exam TodoList/main.cpp
@@ -1,3 +1,50 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Task {
+public:
+    Task(std::string description, int date, int priority) {
+        _description = description;
+        _date = date;
+        _priority = priority;
+    }
+
+    std::string getDescription() const { return _description; }
+    int getDate() const { return _date; }
+    int getPriority() const { return _priority; }
+
+private:
+    std::string _description;
+    int _date; // For simplicity, date is an int
+    int _priority; // Also, priority is int for simplicity. Should be enum
+};
+
+class TaskList {
+    virtual std::string render(std::vector<Task*>) const = 0;
+};
+
+class Numbered : public TaskList {
+    std::string render(std::vector<Task*> tasks) const override {
+        std::string rendered = "";
+        int counter = 0;
+        for (auto task : tasks) {
+            rendered += ++counter + ". " + task->getDescription() + " Due: " + std::to_string(task->getDate()) + " Priority: " + std::to_string(task->getPriority()) + "\n";
+        }
+        return rendered;
+    }
+};
+
+class Bulleted : public TaskList {
+    std::string render(std::vector<Task*> tasks) const override {
+        std::string rendered = "";
+        for (auto task : tasks) {
+            rendered += "* " + task->getDescription() + " Due: " + std::to_string(task->getDate()) + " Priority: " + std::to_string(task->getPriority()) + "\n";
+        }
+        return rendered;
+    }
+};
+
 int main() {
     return 0;
 }
